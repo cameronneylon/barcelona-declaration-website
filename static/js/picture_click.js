37684c96bb52b0de4fedf2b30874fc139653b859
Bianca Kramer <bianca.kramer@gmail.com>
2024-04-20 22:15:33 +0200

    replace infographic dark mode image with solid background version

--- static/js/picture_click.js
+++ static/js/picture_click.js
@@ -1,6 +1,6 @@
 function doClick() {
    if(window.matchMedia && window.matchMedia("(prefers-color-scheme:dark)").matches)
-       window.open("/images/open_research_information_infographic_darkmode.png");
+       window.open("/images/open_research_information_infographic_darkmode_full.png");
    else
       window.open("/images/open_research_information_infographic.jpg");
        
